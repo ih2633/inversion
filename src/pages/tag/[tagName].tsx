58420018760db2c6fe303fc2f83e630bd2fa9815
ih2633 <horie2633@gmail.com>
2022-11-22 17:04:48 +0900

    [searchWords] change

--- src/pages/tag/[tagName].tsx
+++ src/pages/tag/[tagName].tsx
@@ -1,4 +1,5 @@
 import { type NextPage } from "next";
+import { useEffect, useState } from "react";
 import { trpc } from "@/utils/trpc";
 import Cards from "@/components/article/Cards";
 import { useRouter } from "next/router";
@@ -6,17 +7,56 @@ import { useSelectCategory } from "@/hooks/selectCategory";
 import SelectCategoryButton from "@/components/article/SelectCategoryButton";
 import Pagenation from "@/components/Pagenation";
 
+type ResultInfo = {
+  tagName: string
+  skip: string
+  take: string
+}
+
+type PagenationInfo = {
+  skip: string
+  take: string
+}
+
+const initData = {
+  tagName: "",
+  skip: "0",
+  take: ""
+}
+
+const initPagenationData = {
+  skip: "0",
+  take: ""
+}
+
+const isPageInfo = (data: unknown): data is ResultInfo => {
+  if (data == null) {
+    return false;
+  }
+  const d = data as Record<string, unknown>
+  return typeof d.tagName === "string" && typeof d.skip === "string" && typeof d.take === "string";
+}
+
 const TagList: NextPage = () => {
   const [selectCategory, filterCategory] = useSelectCategory();
+  const [resultInfo, setResultInfo] = useState<ResultInfo>(initData);
+  const [pagenationInfo, setPagenationInfo] = useState<PagenationInfo>(initPagenationData)
+  const [isReady, setIsReady] = useState<boolean>(false)
   const router = useRouter();
   
-  const tagName = router.query.tagName as string;
-  const skip = router.query.skip as string;
-  const take = router.query.take as string;
+  useEffect(() => {
+    if (router.isReady) {
+      const query = router.query
+      console.log(query)
+      if (isPageInfo(query)) {
+        setResultInfo(query)
+        setPagenationInfo({ skip: query.skip, take: query.take })
+        setIsReady(true)
+      }
+    }
+  },[router])
 
-  const page = { skip: skip, take: take };
-
-  const { data: articles, isSuccess } = trpc.article.selectArticleTag.useQuery<string>({tagName, skip, take },{enabled: router.isReady});
+  const { data: articles, isSuccess, isLoading } = trpc.article.selectArticleTag.useQuery<ResultInfo>(resultInfo, { enabled: isReady });
 
 
   return (
@@ -28,7 +68,7 @@ const TagList: NextPage = () => {
         <div className="col-span-4 border-x-2">
           <div className="m-12">
             <div className="mb-4 items-center space-x-5">
-              <p className="prose-xl prose mb-5">#{tagName} 検索結果</p>
+              <p className="prose-xl prose mb-5">#{resultInfo.tagName} 検索結果</p>
               <SelectCategoryButton
                 selectCategory={selectCategory}
                 filterCategory={filterCategory}
@@ -40,8 +80,11 @@ const TagList: NextPage = () => {
                 selectCategory={selectCategory}
               />
             )}
+            {!isLoading && !articles ? <div>Not Data</div> :
+            <div></div>}
+            
           </div>
-          <Pagenation page={page} />
+          <Pagenation page={pagenationInfo} />
         </div>
         <div className="col-span-2"></div>
       </div>
